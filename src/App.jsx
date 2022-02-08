ff4f7d0acc24222766c71d86e74608647b067e0b
andre <markues@protonmail.com>
2022-02-08 11:27:33 -0300

    change color logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,20 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  let nColor = '';
+  if (count > 0) {
+    nColor = 'green';
+  } else if (count < 0) {
+    nColor = 'red';
+  } else {
+    nColor = 'white';
+  }
 
   return (
     <section className='counter-section'>
       <div className='container-counter'>
         <h1 style={{fontSize: '40px'}}>Counter App</h1>
-        <p id='counter'>{count}</p>
+        <p id='counter' style={{color: nColor}}>{count}</p>
         <div className="buttons">
           <button onClick={() => setCount(count - 1)}>- Subtract</button>
           <button onClick={() => setCount(0)}>! Reset</button>
